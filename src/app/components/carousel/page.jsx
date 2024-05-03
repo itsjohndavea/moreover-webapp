83e8c6688c605521ee3e43f6996ecbc0e05b1acd
myst <johndaveaquino35@gmail.com>
2024-05-03 15:55:53 +0800

    Update page.jsx

--- src/app/components/carousel/page.jsx
+++ src/app/components/carousel/page.jsx
@@ -10,30 +10,13 @@ const Carousel = () => {
   return (
     <>
       <div className=" w-screen flex-col items-center text-center">
-        <Image
+        {/* <Image
           className="w-full h-full object-cover object-center"
           src={banner[page]}
-          sizes="100vw"
           width={1000}
           height={1000}
           alt={`banner${page}`}
-        ></Image>
-        <button
-          className="absolute h-17 rounded-md left-0 w-20 bg-black opacity-50 text-white"
-          onClick={() =>
-            setPage(
-              (prevPage) => (prevPage - 1 + banner.length) % banner.length,
-            )
-          }
-        >
-          Previous
-        </button>
-        <button
-          className="absolute rounded-sm h-17 right-0 bottom-0 w-12 bg-black opacity-50 text-white"
-          onClick={goToNextPage}
-        >
-          Next
-        </button>
+        ></Image> */}
       </div>
     </>
   );
