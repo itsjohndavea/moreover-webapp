524de54cb481c85c4e8517edb892b0d1f6d845e6
myst <johndaveaquino35@gmail.com>
2024-05-03 15:48:35 +0800

    Create page.jsx

---
+++ src/app/components/sale/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sale = () => {
+  return (
+    <div>
+      <div className="hidden lg:flex flex h-15 w-screen bg-slate-600 text-white justify-center">
+        <p>🛒50% Discount!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sale;
