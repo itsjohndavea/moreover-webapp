e2bf18c57c64b6f80105fb375b53a7e569f1198e
myst <johndaveaquino35@gmail.com>
2024-05-04 15:44:52 +0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,33 @@
-import Carousel from "./components/carousel/page";
-import Footer from "./components/footer/page";
-import Navbar from "./components/navbar/page";
+"use client";
+
+import { useState, useEffect, Suspense } from "react";
 import Sale from "./components/sale/page";
+import Navbar from "./components/navbar/page";
+import ProductsPage from "./components/productspage/page";
+import Footer from "./components/footer/page";
+
+async function getData() {
+  const res = await fetch("https://fakestoreapi.com/products/");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
 export default function Home() {
+  const [fetchedProducts, setFetchedProducts] = useState([]);
+
+  useEffect(() => {
+    getData()
+      .then((data) => setFetchedProducts(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <>
       <Sale />
       <Navbar />
-      <Carousel />
+      <ProductsPage products={fetchedProducts} />
       <Footer />
     </>
   );
